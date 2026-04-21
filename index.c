2aa9ec91ff0d1a2c8245687cd7a79f0ae01b0685
JAYA SURYA <purimetlasuryantr@gmail.com>
2026-04-21 14:39:40 +0530

    Update index.c

--- index.c
+++ index.c
@@ -1,12 +1,5 @@
 // index.c — Staging area implementation
 
-//index.c — Staging area implementation
-
-//index.c — Staging area implementation
-
-// index.c — Staging area implementation
-
-// index.c — Staging area implementation
 
 // Text format of .pes/index (one entry per line, sorted by path):
 //
